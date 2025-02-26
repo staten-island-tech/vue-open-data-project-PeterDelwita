eaad4273dd9a89ca4de157409a95170a16357408
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-02-26 14:02:28 -0500

    a

---
+++ app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!--API: https://data.cityofnewyork.us/resource/mifw-tguq.json-->
+  <h1 class="text-blue-500 text-center text-[64px] font-bold">Voting/Poll Sites in NYC</h1>
+</template>
+
+<style scoped></style>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-blue-500">aaaa</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
