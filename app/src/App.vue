77c5593d45799207805c5fecdbb33f80454f1004
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-03-04 14:01:55 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// At least 2 charts
+// At least 2 charts (use chartjs)
+
+// Process
+// 1. Make the async function and fetch data
+// 2. Decide which parts of the data should be used to make charts
+// 2. Look at the API and use the data to make a component
 </script>
 
 <template>
-  <!--API: https://data.cityofnewyork.us/resource/mifw-tguq.json-->
+  <!--API: https://data.cityofnewyork.us/resource/mifw-tguq.json -->
   <h1 class="text-blue-500 text-center text-[64px] font-bold">Voting/Poll Sites in NYC</h1>
 </template>
 
