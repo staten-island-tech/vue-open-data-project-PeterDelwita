73cc7bcbea103d28112d3fef401cfba192a0e479
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-03-06 13:02:27 -0500

    Tailwind is the bestest

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+import { ref, onMounted } from 'vue'
+const day = ref('')
+async function getCasesPerDay() {
+  try {
+    let res = await fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json')
+    let data = await res.json()
+    day.value = data.results
+  } catch (error) {
+    alert('Date not found')
+  }
+}
 // At least 2 charts (use chartjs)
 
 // Process
 // 1. Make the async function and fetch data
 // 2. Decide which parts of the data should be used to make charts
-// 2. Look at the API and use the data to make a component
+// 3. Look at the API and use the data to make components
 </script>
 
 <template>
-  <!--API: https://data.cityofnewyork.us/resource/mifw-tguq.json -->
-  <h1 class="text-blue-500 text-center text-[64px] font-bold">Voting/Poll Sites in NYC</h1>
+  <!--API: https://data.cityofnewyork.us/resource/rc75-m7u3.json -->
+  <h1 class="text-red-500 text-center text-[64px] font-bold">
+    COVID-19 Daily Counts of Cases, Hospitalizations, and Deaths
+  </h1>
 </template>
 
 <style scoped></style>
