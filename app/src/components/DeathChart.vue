13e03671fd623023b221c1250911c51f8e893494
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-03-20 13:59:51 -0400

    a

---
+++ app/src/components/DeathChart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>COVID Deaths Per Day</h2>
+  </div>
+</template>
+
+<script setup>
+import { reactive, onMounted } from 'vue'
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+</script>
+
+<style scoped></style>
